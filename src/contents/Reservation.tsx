73068932ee636509cc7173c9c2c33774c3747351
Léa Baty <leabaty.dev@gmail.com>
2023-11-27 21:24:46 +0100

    Add checklist and trip details

--- src/contents/Reservation.tsx
+++ src/contents/Reservation.tsx
@@ -1,15 +1,77 @@
 import LocalizedStrings from 'react-localization';
 
+import {
+  AgeLogo,
+  DaysLogo,
+  PaxLogo,
+  ProgramLogo,
+  WhenLogo,
+  WhereLogo,
+  CheckListLogo,
+} from './images/reservationLogos';
+
 const lang = new LocalizedStrings({
   en: {
-    title: 'Reservation',
+    title: 'Your trip',
+    pax: '1 to 6 passengers',
+    age: 'from 18 to 99 years old',
+    days: 'from Monday to Friday',
+    where: 'Blue coast, Frioul, Marseille',
+    when: 'Boarding at 10 am',
+    winterProgram: 'lunch in a port or calanque',
+    summerProgram: 'swimming & lunch in a calanque',
+    checkListTitle: 'Checklist',
+    checkList: [
+      'waterproof clothes',
+      'soft-soled shoes (such as sneakers or rain boots)',
+      'snack',
+      'sunscreen',
+    ],
+    note: "For our safety, all outings will be based on the day's weather report. Depending on the weather conditions, the captain may postpone the navigation or propose a non-stop navigation.",
   },
 
   fr: {
-    title: 'Réservation',
+    title: 'Votre sortie',
+    pax: '1 à 6 passagers',
+    age: 'de 18 à 99 ans',
+    days: 'du lundi au vendredi',
+    where: 'Côte bleue, Frioul, Marseille',
+    when: 'Embarquement à 10h',
+    winterProgram: 'déjeuner dans un port ou une calanque',
+    summerProgram: 'baignade et déjeuner dans une calanque',
+    checkListTitle: 'Check-List',
+    checkList: [
+      'vêtements imperméables',
+      'chaussures à semelle souple (comme des baskets ou des bottes de pluie)',
+      'casse-croûte',
+      'crème solaire',
+    ],
+    note: 'Pour notre sécurité, toutes les sorties seront basées sur le bulletin météo du jour. Selon les conditions météorologiques, le capitaine peut reporter la navigation ou proposer une navigation sans escale.',
   },
 });
 
+const getCurrentProgram = () => {
+  const currentMonth = new Date().getMonth() + 1;
+
+  if (currentMonth >= 5 && currentMonth <= 10) {
+    return { name: lang.summerProgram, img: ProgramLogo };
+  } else {
+    return { name: lang.winterProgram, img: ProgramLogo };
+  }
+};
+
 export const content = {
   title: lang.title,
+  tiles: [
+    { name: lang.pax, img: PaxLogo },
+    { name: lang.age, img: AgeLogo },
+    { name: lang.days, img: DaysLogo },
+    { name: lang.where, img: WhereLogo },
+    { name: lang.when, img: WhenLogo },
+    getCurrentProgram(),
+  ],
+  checkListTitle: lang.checkListTitle,
+  CheckListLogo,
+  checkList: lang.checkList,
+  note: lang.note,
 };
--- src/pages/Reservation.tsx
+++ src/pages/Reservation.tsx
@@ -1,13 +1,44 @@
 import React from 'react';
 
+import '../styles/Reservation.scss';
+
 import { content } from '../contents/Reservation';
 
 import Form from '../components/Form/Form';
+import { CheckListLogo } from '../contents/images/reservationLogos';
 
 export default function Reservation() {
+  const logoColor = '#fff';
   return (
     <div className='page'>
       <h1>{content.title}</h1>
+      <div className='res-tiles'>
+        {content.tiles.map((item, index) => (
+          <>
+            <div className='res-tile-header' key={index}>
+              <div className='res-img'>
+                {React.createElement(item.img, {
+                  color: logoColor,
+                  size: '30',
+                })}
+              </div>
+              <div className='res-tile'>
+                <p className='res-text'>{item.name}</p>{' '}
+              </div>
+            </div>
+          </>
+        ))}
+      </div>
+      <div className='res-checklist'>
+        <h2>{content.checkListTitle}</h2>
+        {content.checkList.map((item, index) => (
+          <div key={index} className='res-checklist-item'>
+            <CheckListLogo color='#001f36' size='20' />
+            <p className='res-checklist-txt'>{item}</p>
+          </div>
+        ))}
+      </div>
+
       <Form />
     </div>
   );
